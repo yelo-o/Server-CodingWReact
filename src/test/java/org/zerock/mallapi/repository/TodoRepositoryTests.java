154df668ea431a5b5177004fd5d9fb542f457700
yelo-o <flyordig@gmail.com>
2024-02-11 00:34:37 +0900

    insert 문 100개 실행 테스트 -> db tool에서 확인 가능

--- src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
+++ src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -1,8 +1,11 @@
 package org.zerock.mallapi.repository;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mallapi.domain.Todo;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -14,9 +17,16 @@ public class TodoRepositoryTests {
     private TodoRepository todoRepository;
 
     @Test
-    public void test1() {
-        log.info("----------------------------");
-        log.info(todoRepository);
+    public void testInsert() {
+        for (int i = 1; i <= 100; i++) {
+            Todo todo = Todo.builder()
+            .title("Title..." + i)
+            .dueDate(LocalDate.of(2023,12,31))
+            .writer("user00")
+            .build();
+
+            todoRepository.save(todo);
+        }
     }
     
 }
